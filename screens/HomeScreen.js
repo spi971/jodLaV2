f9e7db1ee2c4f9d147b1726b393b4512aa9c758c
spi971 <sandypierre@protonmail.com>
2023-11-15 10:58:46 +0100

    Implement home page style && behaviors

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,7 +1,17 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
-import { Image, Text, TextInput, TouchableOpacity, View } from "react-native";
-import { MagnifyingGlassIcon } from "react-native-heroicons/outline";
+import {
+  Image,
+  ScrollView,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import {
+  CalendarDaysIcon,
+  MagnifyingGlassIcon,
+} from "react-native-heroicons/outline";
 import { MapPinIcon } from "react-native-heroicons/solid";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { theme } from "../theme";
@@ -10,8 +20,8 @@ export default function HomeScreen() {
   const [showSearch, setShowSearch] = useState(false);
   const [locations, setLocations] = useState([1, 2, 3]);
   const handleLocation = (location) => {
-    console.log(location)
-  }
+    console.log(location);
+  };
 
   return (
     <View className='flex-1 relative'>
@@ -22,6 +32,7 @@ export default function HomeScreen() {
         className='absolute h-full w-full'
       />
       <SafeAreaView className='flex flex-1'>
+        {/* Search bar */}
         <View style={{ height: "7%" }} className='mx-4 relative z-50'>
           <View
             style={{
@@ -53,17 +64,126 @@ export default function HomeScreen() {
                   : "";
                 return (
                   <TouchableOpacity
-                  onPress={()=> handleLocation(location)}
+                    onPress={() => handleLocation(location)}
                     key={index}
-                    className={'flex-row items-center border-0 p-3 px-4 mb-1 ' + borderClass}
+                    className={
+                      "flex-row items-center border-0 p-3 px-4 mb-1 " +
+                      borderClass
+                    }
                   >
                     <MapPinIcon size='20' color='gray' />
-                    <Text className="text-black text-lg ml-2">Paris, France</Text>
+                    <Text className='text-black text-lg ml-2'>
+                      Paris, France
+                    </Text>
                   </TouchableOpacity>
                 );
               })}
             </View>
           ) : null}
+        </View>
+
+        {/* forecast dispaly */}
+        <View className='flex justify-around mx-4 flex-1 mb-2'>
+          {/* location details  */}
+          <Text className='text-white text-center text-2xl font-bold'>
+            Paris,
+            <Text className='text-gray-400 text-lg font-semibold'>France</Text>
+          </Text>
+          {/* weather image */}
+          <View className='flex-row justify-center'>
+            <Image
+              source={require("../assets/images/cloud.png")}
+              className='w-52 h-52'
+            />
+          </View>
+          {/* temperature */}
+          <View className='space-y-2'>
+            <Text className='text-white text-center font-bold text-6xl ml-5'>
+              12&#176;
+            </Text>
+            {/* weather condition */}
+            <Text className='text-white text-center text-xl ml-5 tracking-widest'>
+              Cloudy
+            </Text>
+          </View>
+          {/* other informations */}
+          <View className='flex-row justify-between mx-4'>
+            {/* wind */}
+            <View className='flex-row space-x-2 items-center'>
+              <Image
+                source={require("../assets/icons/wind.png")}
+                className='h-6 w-6'
+              />
+              <Text className='text-white font-semibold text-base'>22km</Text>
+            </View>
+            {/* humidity */}
+            <View className='flex-row space-x-2 items-center'>
+              <Image
+                source={require("../assets/icons/drop.png")}
+                className='h-6 w-6'
+              />
+              <Text className='text-white font-semibold text-base'>22%</Text>
+            </View>
+            {/* Sunrise */}
+            <View className='flex-row space-x-2 items-center'>
+              <Image
+                source={require("../assets/icons/sun.png")}
+                className='h-6 w-6'
+              />
+              <Text className='text-white font-semibold text-base'>
+                6:05 AM
+              </Text>
+            </View>
+          </View>
+        </View>
+
+        {/* forecast for next day */}
+        <View className='mb-2 space-y-3'>
+          <View className='flex-row items-center mx-5 space-x-2'>
+            <CalendarDaysIcon size='22' color='white' />
+            <Text className='text-white text-base'>Next days</Text>
+          </View>
+          <ScrollView
+            horizontal
+            contentContainerStyle={{ paddingHorizontal: 35 }}
+            showsHorizontalScrollIndicator={false}
+          >
+            <View
+              className='flex justify-center items-center w-24 rounded-3xl py-3 space-y-1 mr-4'
+              style={{ backgroundColor: theme.bgWhite(0.15) }}
+            >
+              <Image
+                source={require("../assets/images/heavyrain.png")}
+                className='h-11 w-11'
+              />
+              <Text className='text-white'>Monday</Text>
+              <Text className='text-white text-xl font-semibold'>16&#176;</Text>
+            </View>
+
+            <View
+              className='flex justify-center items-center w-24 rounded-3xl py-3 space-y-1 mr-4'
+              style={{ backgroundColor: theme.bgWhite(0.15) }}
+            >
+              <Image
+                source={require("../assets/images/heavyrain.png")}
+                className='h-11 w-11'
+              />
+              <Text className='text-white'>Tuesady</Text>
+              <Text className='text-white text-xl font-semibold'>16&#176;</Text>
+            </View>
+
+            <View
+              className='flex justify-center items-center w-24 rounded-3xl py-3 space-y-1 mr-4'
+              style={{ backgroundColor: theme.bgWhite(0.15) }}
+            >
+              <Image
+                source={require("../assets/images/heavyrain.png")}
+                className='h-11 w-11'
+              />
+              <Text className='text-white'>Wednesday</Text>
+              <Text className='text-white text-xl font-semibold'>16&#176;</Text>
+            </View>
+          </ScrollView>
         </View>
       </SafeAreaView>
     </View>
